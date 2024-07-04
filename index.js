21ff9b3756de4a7c34c442b7965005d166e1da73
zahrafeisal <zahrafeisal3@gmail.com>
2024-07-04 15:49:28 +0300

    added arithmetic and assignment operators

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+let num1 = 31
+let num2 = 2
+const multiply = num1 * num2
+
+const random = Math.floor(Math.random() * 10)
+
+let num3 = 10
+let num4 = 6
+const mod = 10 % 6
+
+const max = Math.max(5, 10, 15, 20)
